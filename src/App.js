d4ffcda73597c9ed19413e441f43e9fda4b46943
wissal <wissal.el@gmail.com>
2019-09-25 22:23:47 -0700

    Add Signin and Register forms make the navigation responsive depending if the user is signed in or out

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import SignIn from './components/SignIn/SignIn';
+import Register from './components/Register/Register';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Rank from './components/Rank/Rank'
 import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'f2d8c6a20f994c9e9d8365c40f06a51e'
+ });
 
 const particlesOptions = {
   particles: {
@@ -18,19 +26,77 @@ const particlesOptions = {
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className='particles'
-          params={particlesOptions}
-       />
-       <Navigation />
-       <Logo />
-       <Rank />
-       <ImageLinkForm />
-       {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {input: '', imageUrl: '', box: {}, route: 'signin', isSignedIn: false}
+  }
+
+  
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+  
+  handleChange = e => {
+    this.setState({input: e.target.value})
+  }
+
+  handleSubmit = e => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+        Clarifai.FACE_DETECT_MODEL,
+        this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(err => console.log(err));
+  }
+
+  handleRouteChange = route => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render(){
+    const { imageUrl, box, isSignedIn, route} = this.state;
+    return (
+      <div className="App">
+        <Particles className='particles'
+            params={particlesOptions}
+        />
+        <Navigation isSignedIn={isSignedIn} handleRouteChange={this.handleRouteChange} />
+        {route === 'home'
+         ? <>
+            <Logo />
+            <Rank />
+            <ImageLinkForm handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+           </>
+         : (
+           route === 'signin'
+           ? <SignIn handleRouteChange={this.handleRouteChange}/>
+           : <Register handleRouteChange={this.handleRouteChange}/>
+         )
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({handleChange, handleSubmit}) => {
     return (
        <div className='f3'>
            <p>
@@ -9,8 +9,8 @@ const ImageLinkForm = () => {
            </p>
            <div className='center'>
               <div className='form center pa4 br3 shadow-5'>
-                <input className='f4 pa2 w-70 center' type='text'/>
-                <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
+                <input className='f4 pa2 w-70 center' type='text' onChange={handleChange}/>
+                <button onClick={handleSubmit} className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>Detect</button>
               </div>
            </div>
        </div>
