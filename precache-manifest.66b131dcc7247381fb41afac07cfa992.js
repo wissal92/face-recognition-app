f2bc9a70314e6b1f38bb181c19f42d0e45cc25b5
wissal <wissal.el@gmail.com>
2019-09-29 22:13:06 -0700

    Updates

---
+++ precache-manifest.66b131dcc7247381fb41afac07cfa992.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36169709be3cfb1afe319be0de992ab6",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "3a018f4e023ab7597d43",
+    "url": "/facerecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b4afb1b054874d73a2af",
+    "url": "/facerecognition/static/css/main.ddbf8c85.chunk.css"
+  },
+  {
+    "revision": "3a018f4e023ab7597d43",
+    "url": "/facerecognition/static/js/2.c6b1402f.chunk.js"
+  },
+  {
+    "revision": "b4afb1b054874d73a2af",
+    "url": "/facerecognition/static/js/main.5fdd1e49.chunk.js"
+  },
+  {
+    "revision": "261fac71c5add8a86a3a",
+    "url": "/facerecognition/static/js/runtime-main.c7f062a9.js"
+  }
+]);
